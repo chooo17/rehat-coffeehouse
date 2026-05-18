e8c9d52df7bb7a87de267a4b824556a632721111
choirrozikin17 <choirrozikin17@gmail.com>
2026-05-18 16:21:16 +0700

    feat: scaffold Next.js 14 project with all dependencies

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+})
